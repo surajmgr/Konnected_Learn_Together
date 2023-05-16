e23cbbc7a5948e7fdfe0bc817f450f8851ce6ab8
Suraj Pulami <surajpulami8@gmail.com>
2023-05-16 10:23:31 +0545

    multipleCORS

--- server/index.js
+++ server/index.js
@@ -9,9 +9,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 const cors = require('cors');
+var allowedDomains = [process.env.client_KED, 'http://localhost:3000'];
 app.use(cors({
-    origin: 'http://localhost:3000/',
-    credentials: true
+  origin: function (origin, callback) {
+    // bypass the requests with no origin (like curl requests, mobile apps, etc )
+    if (!origin) return callback(null, true);
+ 
+    if (allowedDomains.indexOf(origin) === -1) {
+      var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
 }));
 
 const session = require('express-session');
